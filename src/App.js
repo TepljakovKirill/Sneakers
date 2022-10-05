b90f7dc6dcbb513dad252b344b118e55e72dc2f8
Kirill Tepljakov <super.css2017@yandex.ru>
2022-10-05 19:02:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Тепляков - МОЛОДЕЦ!!!! Наконец-то настроил себе рабочее место!!
+    </div>
+  );
+}
+
+export default App;
