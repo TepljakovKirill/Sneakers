71a254cc7b48b9f2fbe3d1f0b1ca5f512c8b946d
Kirill Tepljakov <super.css2017@yandex.ru>
2022-10-06 10:31:31 +0300

    two commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      Тепляков - МОЛОДЕЦ!!!! Наконец-то настроил себе рабочее место!!
+    <div className="wrapper">
+      <header>
+        <div className="headerLogo">
+          <img width={40} height={40} src="/img/logo.svg" alt="логотип" />
+          <div className="headerDescr">
+            <h2>React sneakers</h2>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <div className="headerBasket">
+          <img width={18} height={18} src="/img/cart.svg" alt="логотип" />
+          <span>1205 руб.</span>
+          <img width={18} height={18} src="/img/user.svg" alt="логотип" />
+        </div>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
     </div>
   );
 }
