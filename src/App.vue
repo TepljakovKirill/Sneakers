844bfbb5685261173ca8ad1632a120757ac3a8eb
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2025-08-05 16:17:34 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <MyComponent />
+</template>
+
+<style scoped></style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+
+
+
+</script>
+
+
+
+<template>
+
+  <div>
+    <div class="mb-40">
+      <h2 class="bg-red-500 text-white p-4">Кроссовки Puma</h2>
+      <h2>Цена 20567 руб.</h2>
+    </div>
+  </div>
+</template>
