cf48db4db56e76581667032ce4b20a9a53349b74
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2025-08-06 09:06:53 +0300

    add component Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-
-
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <div>
-    
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
   </div>
 </template>
 
