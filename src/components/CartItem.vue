b94c56d3e9d375eaaf821abfb1fbc7a6fec909fb
KIRILL TEPLYAKOV <49129331+TepljakovKirill@users.noreply.github.com>
2025-08-07 09:19:33 +0300

    add Drawer

---
+++ src/components/CartItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class='flex border items-center border-slate-100 p-4 rounded-xl gap-4 justify-between mb-4'>
+    <img class='w-16 h-16' src="/sneakers/sneakers-1.jpg" alt="sneakers">
+
+    <div class='flex flex-col'>
+      <p>Кроссовки Nike</p>
+
+      <div class='flex justify-between mt-2'>
+        <b>12990 р.</b>
+        <img class='opacity-40 hover:opacity-100 cursor-pointer transition' src="/close.svg" alt="">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped></style>
